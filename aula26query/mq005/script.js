d3abbd640bb0d29da6282011488a1a2a7a9fea76
filipemilk <filipeleche@gmail.com>
2024-01-19 19:23:20 -0300

    Initial commit

---
+++ aula26query/mq005/script.js
@@ -0,0 +1,17 @@
+var menu = window.document.querySelector("menu#menu")
+
+function abrirMenu() {
+    if (menu.style.display == "block") {
+        menu.style.display = "none"
+    } else {
+        menu.style.display = "block"
+    }
+}
+
+function mudouTamanho() {
+    if (window.innerWidth >= 768) {
+        menu.style.display = "block"
+    } else {
+        menu.style.display = "none"
+    }
+}
